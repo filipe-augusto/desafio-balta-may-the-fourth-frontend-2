cc2ea0958f3bc3bdd81f260a2b1711c252fa0e48
FilipeAugusto <filipe@iaux.com.br>
2024-05-01 22:13:30 -0300

    adicionando docs

---
+++ Starls.Assets/docs/wwwroot/js/cursor.js
@@ -0,0 +1,18 @@
+﻿var starWarsCursor = document.querySelector('.star-wars-cursor');
+var trail = document.querySelector('.trail');
+
+document.addEventListener('mousemove', function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    starWarsCursor.style.left = x + 'px';
+    starWarsCursor.style.top = y + window.scrollY + 'px';
+
+   
+    trail.style.left = x - 10 + 'px'; 
+    trail.style.top = y + window.scrollY - 10 + 'px'; 
+    trail.style.opacity = 1; 
+});
+
+setInterval(function () {
+    trail.style.opacity = 0;
+}, 100);
